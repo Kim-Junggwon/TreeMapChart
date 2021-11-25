d6e6f10c4ae1b2e43e0da99ddafc4b028803854d
Kim-Junggwon <94525599+Kim-Junggwon@users.noreply.github.com>
2021-11-25 14:59:00 +0900

    [modify] DB 연동 수정 - 진행중

--- ExpressCode/router/main.js
+++ ExpressCode/router/main.js
@@ -9,34 +9,39 @@ var connection = mysql.createConnection({
 	dateStrings : 'date'
 });
 
-module.exports = function(app) {
-	app.get('/', function(req, res) {
-		// res.render('index.html');
+// var i = 0;
+var a = "";
 
-		connection.query('SELECT * FROM traffic', function(error, result, fields) {
-			if (error) {
-				console.log('Error: ', error);
-			}
-
-			var date = [],
-				category = [],
-				host = [];
-
-			for (var i in result) {
-				date[i] = result[i].date;
-				category[i] = result[i].category;
-				host[i] = result[i].host;
-			}
-
-			res.render('index.ejs', {
-				date: date,
-				category: category,
-				host: host
-			});
-		});
-
+var interval = function () {	// 쿼리 호출 및 변수값 정의
+	connection.query('SELECT * FROM TRAFFIC WHERE DATE > NOW() - INTERVAL 15 SECOND', (error, result, fields) => {
+		if (error) {
+			console.log('Error: ', error);
+		}
+		// console.log(result[0]);
+		a = result[result.length - 1];
+		connection.end();     
 	});
+};
+
+var callQuery = function(app) {
+	app.get('/', function(req, res) {
+
+		interval();
+
+		var result = a.date;
+
+		if (result != undefined) {
+			res.render('index.ejs', {result:result});
+			console.log(result);
+			return;
+		} else {
+			res.send("<h1>err</h1>");
+		}
+	});
+
 	app.get('/about', function(req, res) {
 		res.render('about.html');
 	});
 };
+
+module.exports = callQuery;
